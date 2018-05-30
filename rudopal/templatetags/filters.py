a0a0836a86aee7f644c2a5859aaa2386369e8ff1
Adrian Wiśniewski <adrian.wisniewski@empsgroup.com>
2018-05-30 22:21:29 +0200

    repair news

--- rudopal/templatetags/filters.py
+++ rudopal/templatetags/filters.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.filter(is_safe=True)
-    def modulo(num, val):
-        return num % val
+def modulo(num, val):
+    return num % val
